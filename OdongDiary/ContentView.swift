cc05e520ccda98c52f991c990e463657b1ed4c49
odong-Tree <odongnamu.dev@gmail.com>
2022-08-28 18:03:25 +0900

    chore: first commit

---
+++ OdongDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OdongDiary
+//
+//  Created by odongnamu on 2022/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OdongDiary/OdongDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdongDiaryApp.swift
+//  OdongDiary
+//
+//  Created by odongnamu on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct OdongDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
