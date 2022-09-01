bbd64a8c0c822d82caa0f2ec3b73372282785d94
odong-Tree <odongnamu.dev@gmail.com>
2022-09-02 02:33:08 +0900

    feat: TextEditor Container Layout ScrollView Layout과 같아보이도록 코드 추가

--- OdongDiary/View/DiaryEditView/Views/WriteView/WriteView.swift
+++ OdongDiary/View/DiaryEditView/Views/WriteView/WriteView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import UIKit
 
 struct WriteView: View {
     @EnvironmentObject var viewModel: DiaryEditViewModel
@@ -14,6 +15,8 @@ struct WriteView: View {
     
     init() {
         UITextView.appearance().backgroundColor = .clear
+        
+        UITextView.appearance().textContainerInset = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 0)
     }
     
     var body: some View {
@@ -25,6 +28,7 @@ struct WriteView: View {
             ZStack(alignment: .topLeading) {
                 if viewModel.type == .write {
                     TextEditor(text: $viewModel.body)
+                        .lineSpacing(1.9)
                         .background(colorSet.third())
                 } else if viewModel.type == .read {
                     ScrollView {
